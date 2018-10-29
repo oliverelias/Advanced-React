088d5ff3c3abfab3f5450e149f21bb31d07dc98d
Oliver Elias <oliver.elias@outlook.com>
2018-10-29 19:35:29 +1100

    Lesson 7

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,15 +1,70 @@
-import React, { Component } from 'react'
-import Meta from './Meta';
-import Header from './Header';
+import React, { Component } from "react";
+import Meta from "./Meta";
+import Header from "./Header";
+import styled, {ThemeProvider, injectGlobal } from "styled-components";
+
+const theme = {
+  red: "#FF0000",
+  black: "#393939",
+  grey: "#3A3A3A",
+  lightgrey: "#E1E1E1",
+  offWhite: "#EDEDED",
+  maxWidth: "1000px",
+  bs: "0 12px 24px 0 rgba(0, 0, 0, 0.09)"
+};
+
+const StyledPage = styled.div`
+  background: white;
+  color: ${props => props.theme.black};
+`;
+
+const Inner = styled.div`
+  max-width: ${props => props.theme.maxWidth};
+  background: ${({theme}) => theme.red};
+  background: ${props => props.theme.red};
+  margin: 0 auto;
+  padding: 2rem;
+`;
+
+injectGlobal`
+  @font-face {
+    font-family: 'radnica_next';
+    src: url('/static/radnikanext-medium-webfont.woff2')
+    format('woff2');
+    font-weight: normal;
+    font-style: normal;
+  }
+  html {
+    box-sizing: border-box;
+    font-size: 10px;
+  }
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+  body {
+    padding: 0;
+    margin: 0;
+    font-size: 1.5rem;
+    line-height: 2;
+    font-family: 'radnica_next';
+  }
+  a {
+    text-decoration: none;
+    color: ${theme.black};
+    
+  }
+`;
 
 export default class Page extends Component {
   render() {
     return (
-      <div>
-        <Meta />
-        <Header />
-        {this.props.children}
-      </div>
-    )
+      <ThemeProvider theme={theme}>
+        <StyledPage>
+          <Meta />
+          <Header />
+          <Inner>{this.props.children}</Inner>
+        </StyledPage>
+      </ThemeProvider>
+    );
   }
 }
